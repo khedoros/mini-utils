f964af9f3027bc21135442ea2c566fa1058b3249
khedoros <khedoros@gmail.com>
2021-03-18 09:03:17 -0600

    A little code enhancement for flip

--- flip.cpp
+++ flip.cpp
@@ -3,17 +3,35 @@
 
 int main(int argc, char *argv[]) {
     if(argc != 3) {
+        std::cerr<<"Use: "<<argv[0]<<" inputFile outputFile\n";
+        std::cerr<<"\n\tPurpose: Dumb little utility to byte-swap save files output by a Retrode so that they work in an emulator."
+                   "\n\t         Just provide an input filename, and a filename to output the result to.\n\n";
         return 1;
     }
 
     std::ifstream in(argv[1]);
     std::ofstream out(argv[2]);
 
+    if(!in.is_open()) {
+        std::cerr<<"Couldn't open source file "<<argv[1]<<"\n";
+        return 2;
+    }
+
+    if(!out.is_open()) {
+        std::cerr<<"Couldn't open destination file "<<argv[1]<<"\n";
+        return 3;
+    }
+
     char buffer[4];
 
     in.seekg(0, std::ios::end);
     size_t size = in.tellg();
     in.seekg(0, std::ios::beg);
+
+    if(size % 4 != 0) {
+        std::cerr<<"Filesize isn't divisible by 4. Output might not be valid.\n";
+        return 4;
+    }
 
     for(size_t i = 0; i < size; i+=4) {
         in.read(buffer, 4);
@@ -28,6 +46,8 @@ int main(int argc, char *argv[]) {
 
     in.close();
     out.close();
+
+    std::cout<<"Output "<<size<<" bytes to file "<<argv[2]<<".\n";
 
     return 0;
 }
