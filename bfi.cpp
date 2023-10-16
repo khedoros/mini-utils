081dbb5552c5a1a54b2eaa0f116743bebebfe025
khedoros <khedoros@gmail.com>
2023-10-16 01:15:21 -0700

    Reduce size of BF interpreter

--- bfi.cpp
+++ bfi.cpp
@@ -1,64 +1,53 @@
 #include<iostream>
 #include<fstream>
 #include<cstdint>
-#include<cassert>
 #include<vector>
-
-using namespace std;
-int interpret(uint16_t, vector<uint8_t>&, vector<uint8_t>&);
-
+int interpret(uint16_t, std::vector<uint8_t>&, std::vector<uint8_t>&);
 uint16_t mptr = 0;
-
 int main(int argc, char *argv[]) {
-    assert(sizeof(uint16_t) == 2);
-    ifstream in;
-    vector<uint8_t> dat(64*1024,0);
-    vector<uint8_t> prg(64*1024-1,0);
+    std::ifstream in;
+    std::vector<uint8_t> dat(64*1024,0);
+    std::vector<uint8_t> prg(64*1024-1,0);
     size_t filesize = 0;
     if(argc == 2) {
         in.open(argv[1]);
         if(!in.is_open()) {
-            cerr<<"Couldn't open file "<<argv[1]<<", exiting."<<endl;
+            std::cerr<<"Couldn't open file "<<argv[1]<<", exiting."<<std::endl;
             return 1;
         }
     }
     else {
-        cerr<<"Provide a single filename as an argument."<<endl;
+        std::cerr<<"Provide a single filename as an argument."<<std::endl;
         return 1;
     }
-    in.seekg(0,ios::end);
+    in.seekg(0,std::ios::end);
     filesize = in.tellg();
-    in.seekg(0,ios::beg);
+    in.seekg(0,std::ios::beg);
     if(filesize > prg.size()) {
-        cerr<<"I don't believe that you *actually* have a BF program over 64k in size. Exiting."<<endl;
+        std::cerr<<"I don't believe that you *actually* have a BF program over 64k in size. Exiting."<<std::endl;
         return 1;
     }
     prg.resize(filesize);
     in.read(reinterpret_cast<char *>(&(prg[0])), filesize);
-
     uint16_t iptr = 0;
     interpret(iptr, dat, prg);
     return 0;
 }
 
-int interpret(uint16_t iptr, vector<uint8_t>& dat, vector<uint8_t>& prg) {
+int interpret(uint16_t iptr, std::vector<uint8_t>& dat, std::vector<uint8_t>& prg) {
     uint16_t orig_iptr = iptr;
     while(iptr+1 < prg.size()) {
-        //if(prg[iptr] == '>' ||prg[iptr] == '<' ||prg[iptr] == '+' ||prg[iptr] == '-' ||prg[iptr] == '.' ||prg[iptr] == ',' ||prg[iptr] == '[' ||prg[iptr] == ']')
-        //        cout<<"iptr: "<<iptr<<" (inst: "<<prg[iptr]<<"), mptr: "<<mptr<<" (data: "<<uint32_t(dat[mptr])<<")"<<endl;
         switch(prg[iptr]) {
             case '>': mptr++; break;
             case '<': mptr--; break;
             case '+': dat[mptr]++; break;
             case '-': dat[mptr]--; break;
-            case '.': cout<<dat[mptr]; break;
+            case '.': std::cout<<dat[mptr]; break;
             case ',': dat[mptr] = 10; 
-                      while(dat[mptr] == 10) dat[mptr] = cin.get(); 
-                      //cout<<"Got char "<<uint32_t(dat[mptr])<<endl;
+                      while(dat[mptr] == 10) dat[mptr] = std::cin.get(); 
                       break;
             case '[': 
                       if(!dat[mptr]) { //Skip the loop block
-                          //cout<<"Skip loop"<<endl;
                           uint8_t count = 1;
                           while(iptr+1 < prg.size() && count > 0) {
                               iptr++;
@@ -66,22 +55,13 @@ int interpret(uint16_t iptr, vector<uint8_t>& dat, vector<uint8_t>& prg) {
                               else if(prg[iptr] == ']') count--;
                           }
                       }
-                      else { //Interpret the loop block
-                          //cout<<"Enter loop"<<endl;
-                          iptr = interpret(iptr+1, dat, prg);
-                      }
+                      else iptr = interpret(iptr+1, dat, prg);
                       break;
             case ']': 
-                      if(!dat[mptr]) { //Exit end of loop
-                          //cout<<"Exit loop"<<endl;
-                          return iptr; //Next thing to happen will be for the iptr to be incremented.
-                      }
-                      else { //Return to beginning of loop
-                          //cout<<"Iterate loop"<<endl;
-                          iptr = orig_iptr-1; //Because it's going to increment
-                      }
+                      if(!dat[mptr]) return iptr; //Next thing to happen will be for the iptr to be incremented.
+                      else iptr = orig_iptr-1; //Because it's going to increment
                       break;
-            default: break; //cerr<<"Invalid character found)"<<endl; return (1024*64)-1;
+            default: break;
         }
         iptr++;
     }
